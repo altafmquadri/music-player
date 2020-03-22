c43a648cc8124495be9449408d8102dd94b4c59c
Altaf Quadri <altafmquadri@gmail.com>
2020-03-22 16:32:42 -0400

    completed

--- script.js
+++ script.js
@@ -1,3 +1,107 @@
 /* music from 
 https://www.bensound.com/ */
 
+const musicContainer = document.getElementById('music-container')
+
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress-container')
+
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
+
+
+//song titles
+const songs = ['hey', 'summer', 'ukulele']
+
+//keep track of song
+let songIndex = 2
+
+
+//update song details 
+const loadSong = (song) => {
+    title.innerText = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+//play song
+const playSong = () => {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+    audio.play()
+}
+//pause song
+const pauseSong = () => {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    audio.pause()
+}
+
+//go back one song
+const prevSong = () => {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+//go to next song
+const nextSong = () => {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+//update
+const updateProgress = (e) => {
+    const { duration, currentTime } = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+//set progress bar
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+}
+
+//event listeners 
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if (isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+//change song
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+//time/song update
+audio.addEventListener('timeupdate', updateProgress)
+
+//click on progress bar
+progressContainer.addEventListener('click', setProgress)
+
+//song ends
+audio.addEventListener('ended', nextSong)
+
+//Initially load song details into DOM
+loadSong(songs[songIndex])
